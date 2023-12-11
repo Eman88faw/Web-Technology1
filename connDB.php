1528abaee03c88b15a75bbf377638e4fbe2cfe99
Eman88faw <132667414+Eman88faw@users.noreply.github.com>
2023-12-11 12:44:29 +0100

    connDB
    Testen Sie die Datenbankverbindung

---
+++ connDB.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "EmaFaw";
+$password = "123456";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=art", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ 
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+?>
